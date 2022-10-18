b478b2ab2bdb5cda983411ea4646336e7759a43b
«Anastasiya» <nastjakoka@mail.ru>
2022-10-18 10:54:01 +0300

    Задача 4

--- DZ_4/Zadacha4.py
+++ DZ_4/Zadacha4.py
@@ -2,46 +2,49 @@
 # многочлена и записать в файл многочлен степени k
 # k - максимальная степень многочлена, следующий степень на 1 меньше и так до ноля
 # Коэффициенты расставляет random, поэтому при коэффициенте 0 просто пропускаем данную итерацию степени
+
 # Пример:
 # k=2 -> 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10x² = 0
 # k=5 -> 3x⁵ + 5x⁴ - 6x³ - 3x = 0
 
-from random import randint
-  
-k = int(input("Введите натуральную степень k: "))
+import random
 
-path1 = 'pol1.txt'
-path2 = 'pol2.txt'
 
-def rand_coef():
-    return randint(-100,100)
+def write_file(st):
+    with open('fileHomework4_4.txt', 'w') as data:
+        data.write(st)
 
-def create_polymonial(k):
-    string = ''
-    for i in range(k, -1, -1):
-        coef = rand_coef()
-        if len(string) == 0 and coef != 0:
-            string += f"{coef}*x^{i}"
-        elif i not in [0,1] and coef != 0:
-            if coef > 0:
-                string += f" + {coef}*x^{i}"
-            else:
-                string += f" - {abs(coef)}*x^{i}"
-        elif i == 1 and coef != 0:
-            if coef > 0:
-                string += f" + {coef}*x"
-            else:
-                string += f" - {abs(coef)}*x"            
-        elif i == 0 and coef != 0:
-            if coef > 0:
-                string += f" + {coef} = 0"
-            else:
-                string += f" - {abs(coef)} = 0" 
-                         
-    return string
 
-with open(path1, 'w', encoding='UTF-8') as data:
-    data.write(create_polymonial(k))
+def rnd():
+    return random.randint(0,101)
 
-with open(path2, 'w', encoding='UTF-8') as data:
-    data.write(create_polymonial(k))
+
+def create_mn(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
+    
+
+def create_str(sp):
+    lst= sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
+
+k = int(input("Введите натуральную степень k = "))
+koef = create_mn(k)
+write_file(create_str(koef))
